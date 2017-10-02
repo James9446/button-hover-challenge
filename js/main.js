1abc1d454e4cf8770ab5acab0193d1632abe56e0
James Reynolds <jameshreynolds@gmail.com>
2017-10-02 13:14:49 -0700

    Using improved solution and an additinal button.

--- js/main.js
+++ js/main.js
@@ -6,31 +6,38 @@
 
 // (3) make the OPPOSITE buttons switch color scheme when you hover over it's counterpart (so when you hover over the darker button, it turns lighter while the lighter button turns darker, and vice versa).
 
-
+// ----==== Original solution for two buttons ====---- //
 // When the page (aka document) is ready (i.e. loaded), do this (function means 'do this'):
-$(document).ready(function() {
-	// When button with class btn1 or btn2 has the mouse enter, do this:
-	$(".btn1, .btn2").mouseenter(function(){
-		// For button with class btn2 it switches the lighter class to the darker class
-		$(".btn2").removeClass("lighter").addClass("darker");
-		// For button with class btn1 it switches the darker class to the lighter class
-		$(".btn1").removeClass("darker").addClass("lighter");
-		// For 'this' i.e. the selected element, add the class bigger
-		$(this).addClass("bigger");
-	});
+// $(document).ready(function() {
+// 	// When button with class btn1 or btn2 has the mouse enter, do this:
+// 	$(".btn1, .btn2").mouseenter(function(){
+// 		// For button with class btn2 it switches the lighter class to the darker class
+// 		$(".btn2").removeClass("lighter").addClass("darker");
+// 		// For button with class btn1 it switches the darker class to the lighter class
+// 		$(".btn1").removeClass("darker").addClass("lighter");
+// 		// For 'this' i.e. the selected element, add the class bigger
+// 		$(this).addClass("bigger");
+// 	});
+// });
+
+// // When the page (aka document) is ready (i.e. loaded), do this (function means 'do this'):
+// $(document).ready(function() {
+// 	// When button is hovered, do this:
+// 	$(".btn1, .btn2").mouseleave(function(){
+// 		// For button with class btn1 it switches the lighter class back to the darker class
+// 		$(".btn1").removeClass("lighter").addClass("darker");
+// 		// For button with class btn2 it switches the darker class back to the lighter class
+// 		$(".btn2").removeClass("darker").addClass("lighter");
+// 		// For 'this' i.e. the selected element, remove the class bigger
+// 		$(this).removeClass("bigger");
+// 	});
+// });
+
+
+// ----==== Now trying for three buttons ====---- //
+$(document).ready(function() { 
+  $("button").hover(function() {
+    $(this).toggleClass("grow lighter darker");
+    $($("button").not(this)).toggleClass("lighter darker"); 
+  }); 
 });
-
-// When the page (aka document) is ready (i.e. loaded), do this (function means 'do this'):
-$(document).ready(function() {
-	// When button is hovered, do this:
-	$(".btn1, .btn2").mouseleave(function(){
-		// For button with class btn1 it switches the lighter class back to the darker class
-		$(".btn1").removeClass("lighter").addClass("darker");
-		// For button with class btn2 it switches the darker class back to the lighter class
-		$(".btn2").removeClass("darker").addClass("lighter");
-		// For 'this' i.e. the selected element, remove the class bigger
-		$(this).removeClass("bigger");
-	});
-});
-
-
